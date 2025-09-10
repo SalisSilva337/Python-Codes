18d21528ac192d126303c07b8b4b0e364e75c7d6
Salis_Silva <salissilvadev@gmail.com>
2025-09-10 15:56:48 -0300

    exercicio github wagner

--- aluno.py
+++
@@ -1,9 +0,0 @@
-from class_aluno import Aluno
-
-nome_aluno = input("Qual o seu nome: ")
-idade_aluno = input("Qual a sua idade: ")
-nota1_aluno = input("Qual a sua primeira nota: ")
-nota2_aluno = input("Qual a sua segunda nota: ")
-
-aluno_escola = Aluno()   
-print("O Aluno",nome_aluno,"de idade", idade_aluno, "anos", aluno_escola.media_aluno())
--- pares.py
+++
@@ -1,9 +0,0 @@
-numero = 1
-
-while numero <=50:
- numero +=1
- if numero %2 == 0:
-     print(numero)
-
- 
- 
